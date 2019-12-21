26cfd47fd625fe32e52159cedb787d3ca0fe5d2e
francelico <samuel.garcin@wecorpindustries.com>
2019-12-22 00:36:02 +0100

    feat: refactored part 3

--- detect_person.py
+++ detect_person.py
@@ -21,24 +21,7 @@ def load_images_from_folder(folder):
             filenames.append(os.path.splitext(os.path.basename(filename))[0])
     return images, filenames
 
-# PART 1: extract the faces
-folder = 'faces'
-known_face_encodings, known_face_names = extract_known_faces(folder)
-
-# PART2: capture webcam frames
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
-
-# PART3: recognise people from video frames
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+def identify_faces(frame, known_face_encodings, known_face_names):
 
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -47,28 +30,27 @@ while True:
     rgb_small_frame = small_frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
 
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
 
-            face_names.append(name)
+        face_names.append(name)
 
-    process_this_frame = not process_this_frame
+    return face_locations, face_names
 
-
+def display_identified(frame, face_locations, face_names):
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
@@ -87,6 +69,27 @@ while True:
 
     # Display the resulting image
     cv2.imshow('Video', frame)
+
+# PART 1: extract the faces
+folder = 'faces'
+known_face_encodings, known_face_names = extract_known_faces(folder)
+
+# PART2: capture webcam frames
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# PART3: recognise people from video frames
+process_this_frame = True
+while True:
+
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Identify faces and display them
+    if process_this_frame: face_locations, face_names = identify_faces(frame, known_face_encodings, known_face_names)
+    display_identified(frame, face_locations, face_names)
+
+    process_this_frame = not process_this_frame
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
